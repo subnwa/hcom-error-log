22844ab7eefdd58d7ba319fc442df28e84e3fbbf
getnwa <nwa.bio.web@gmail.com>
2022-01-28 13:11:20 +0300

    main.js

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 'use strict';
 
-var hcom = require('hcom-error-log');
 
 function error(options) {
     var error_logger = hcom(options)
+    var error = ErrorEvent.function(console.log(error))
+    error_function = function error() {
+        console.log(error(options))
+    }
 }
+
+
+
+console.log(console.error());
+console.log('Error', error);
